2e2d074c2d9535f69027c79d152155d0dbbd4374
MeDaveD <medavedik@yandex.ru>
2016-10-26 23:05:28 +0300

    small visual changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,9 +4,9 @@ lock '3.6.1'
 set :application, 'AzyzaSite'
 set :repo_url, 'https://github.com/medaved/azyzasite.git'
 application = 'AzyzaSite'
-set :rvm_type, :user
+set :rvm_type, :multiuser
 set :rvm_ruby_version, '2.3.0-p0'
-set :deploy_to, '/var/www/apps/AzyzaSite'
+set :deploy_to, '/mnt/temp/www/apps/AzyzaSite'
 
 
 #set :linked_dirs, %w{public/galleries}
@@ -14,8 +14,8 @@ set :deploy_to, '/var/www/apps/AzyzaSite'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+# Default deploy_to directory is /mnt/temp/www/my_app_name
+# set :deploy_to, '/mnt/temp/www/my_app_name'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -45,23 +45,23 @@ namespace :deploy do
   desc 'Setup'
   task :setup do
     on roles(:all) do
-      execute "mkdir  #{shared_path}/config/"
-      execute "mkdir  /var/www/apps/#{application}/run/"
-      execute "mkdir  /var/www/apps/#{application}/log/"
-      execute "mkdir  /var/www/apps/#{application}/socket/"
-      execute "mkdir  /var/www/apps/#{application}/shared/galleries"
-      execute "mkdir #{shared_path}/system"
+    #  execute "mkdir  #{shared_path}/config/"
+     # execute "mkdir  /mnt/temp/www/apps/#{application}/run/"
+      #execute "mkdir  /mnt/temp/www/apps/#{application}/log/"
+      #execute "mkdir  /mnt/temp/www/apps/#{application}/socket/"
+      #execute "mkdir  /mnt/temp/www/apps/#{application}/shared/galleries"
+      #execute "mkdir #{shared_path}/system"
 
       upload!('shared/database.yml', "#{shared_path}/config/database.yml")
       upload!('shared/secrets.yml', "#{shared_path}/config/secrets.yml")
 
 
 
-      upload!('shared/nginx.conf', "#{shared_path}/nginx.conf")
-      sudo 'stop nginx'
-      sudo "rm -f /usr/local/nginx/conf/nginx.conf"
-      sudo "ln -s #{shared_path}/nginx.conf /usr/local/nginx/conf/nginx.conf"
-      sudo 'start nginx'
+      #upload!('shared/nginx.conf', "#{shared_path}/nginx.conf")
+      #sudo 'stop nginx'
+      #sudo "rm -f /usr/local/nginx/conf/nginx.conf"
+      #sudo "ln -s #{shared_path}/nginx.conf /usr/local/nginx/conf/nginx.conf"
+      #sudo 'start nginx'
 
       within release_path do
         with rails_env: fetch(:rails_env) do
